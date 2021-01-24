e2af465ccaba88d33ad50cf8d393cfd93abd625d
Evgenii Morozov <emorozov1101@gmail.com>
2021-01-24 14:45:55 +0500

    Урок 24.01.2021 Функции высшего порядка. Колбек-функции.

--- js/script.js
+++ js/script.js
@@ -89,3 +89,91 @@ console.log('end');
 */
 
 // Именованные callback`и
+/*
+const fs = require('fs');
+
+const print = (fileName, err, data) => {
+    console.log({lines: data.split('\n').length});
+};
+
+const fileName = './function.js';
+const callback = print.bind(null, fileName);
+
+fs.readFile(fileName, 'utf8', callback);
+*/
+
+// Обработчики событий
+/*
+const iterate= (array, listener) => {
+    for (const item of array) {
+        listener(item);
+    }
+};
+
+const cities = ['Kiev', 'London', 'Beijing'];
+
+const print = city => {
+    console.log('City', city);
+};
+iterate(cities, print);
+*/
+
+// Event
+/*
+const adder = initial => {
+    let value = initial; // положили число в value. value будет доступен из замыкания
+    const add = delta => { // создаем функцию add, в которую передаем delta
+        value += delta;
+        if(value >= add.maxValue) add.maxEvent(value);
+        return add; // возврат на саму себя
+    };
+    add.max = (max, event) => {
+        add.maxValue = max;
+        add.maxEvent = event; // саму ссылку на функцию обработки события, сохранили в свойство maxEvent
+        return add; // вернули add из этого max
+    };
+    return add; // снова вернули add из этого сумматора
+};
+
+const maxReached = value => {
+    console.log('max value, value: ' + value);
+};
+
+const a1 = adder(10).max(100, maxReached)(-12);
+
+a1(25);
+a1(50);
+a1(75);
+a1(100);
+a1(-200)(50)(30);
+*/
+
+// Замыкания пример
+// Замыкание - функция замкнула в себе некоторую информацию из области видимости
+/*
+const createPrinter = () => {
+    const name = 'King';
+    const printName = () => {
+        console.log(name);
+    };
+    return printName;
+};
+
+const myPrinter = createPrinter();
+myPrinter(); // King - запомненное значение, при том, что область видимости, где оно было задано больше не существует.
+*/
+
+// Рекурсия - что-то описывается через самого себя, содержит себя в своём же описании.
+/*
+// n! = 1 * 2 * 3 * ... * n
+const factorial = (n) => {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+};
+
+console.log(factorial(4));
+*/
+// базовый случай (точка, в которой нужно остановиться) в нашем примере 1
