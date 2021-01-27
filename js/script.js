83ed92803458874c5e320afd831c6dc9fe607037
Evgenii Morozov <zheka1101@yandex.ru>
2021-01-27 12:53:36 +0500

    Домашнее задание 4 Решение задачи № 3

--- js/script.js
+++ js/script.js
@@ -34,33 +34,11 @@ console.log(value); // 5
 // Задача 3
 
 /*
-3.	Реализуйте функцию contract(fn, ...types) которая оборачивает fn (первый аргумент)
- и проверяет типы аргументов (все аргументы кроме первого и последнего)
- и результата (последний аргумент), гегенрируя исключение TypeError,
-  если типы не совпадают.
-  Как в следующем примере:
-
-const add = (a, b) => a + b;
-const addNumbers = contract(add, Number, Number, Number);
-const res = addNumbers(2, 3);
-console.dir(res); // Output: 5
-
-and
-
-const concat = (s1, s2) => s1 + s2;
-const concatStrings = contract(concat, String, String, String);
-const res = concatStrings('Hello ', 'world!');
-console.dir(res); // Output: Hello world!
- */
-
-/*
 Для решения задачи надо сделать функцию для проверки типа данных элементов
-массива
+ массива
 если элементы одного типа, то возвращает true, иначе false
 
-const checkType = array => {
-    return array.every((el, i, arr) => typeof arr[0] === typeof arr[i]);
-};
+const checkType = array => array.every((el, i, arr) => typeof arr[0] === typeof arr[i]);
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 let arr1 = [1, 2, 3, 'string', 5, 6, 7];
@@ -71,36 +49,30 @@ console.log(checkType(arr)); // true
 console.log(checkType(arr1)); // false
 console.log(checkType(arr2)); // false
 console.log(checkType(arr3)); // true
-
-Также надо доработать функции add и concat( далее concatStrings )
-
-const add = array => array.reduce((result, num) => result + num);
-const concatStrings = array => array.join('');
-
 */
 
-const contract = (fn,...types) => {
-    let array = types;
+const contract = (fn) => {
     const checkType = array => array.every((el, i, arr) => typeof arr[0] === typeof arr[i]);
-
-    const add = array => checkType(array) === true
-        ? array.reduce((result, num) => result + num)
-        : console.log('Type error !!!');
-
-    const concatStrings = array => checkType(array) === true
-        ? array => array.join('')
-        : console.log('Type error !!!');
+    return (...types) => {
+        if (checkType(types) === true) {
+            return fn(...types);
+        }
+        else {
+            throw new TypeError('Invalid arguments !!!');
+        }
+    };
 };
 
-// const add = (a, b) => a + b;
+const add = (a, b) => a + b;
+
 const addNumbers = contract(add);
-const res = addNumbers(2, 3);
-console.dir(res); // Output: 5
+let res = addNumbers(2, 3);
+console.dir(res);
+// 5
 
+const concat = (s1, s2) => s1 + s2;
 
-
-
-
-
-
-
+const concatStrings = contract(concat);
+res = concatStrings('Hello ', 'world!');
+console.dir(res);
+// 'Hello world!'
